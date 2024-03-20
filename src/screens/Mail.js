3dfb657e5bf7b9707f103d04fd3826f9ccfd895a
vinayakkumar9000 <81799193+vinayakkumar9000@users.noreply.github.com>
2024-03-20 17:05:59 +0530

    Update Mail.js

--- src/screens/Mail.js
+++ src/screens/Mail.js
@@ -12,59 +12,37 @@ const Mail = (props) => {
   const [subject, setSubject] = useState();
   const [body, setBody] = useState();
   const [data, setData] = useState([]);
-  const [refreshInterval, setRefreshInterval] = useState(5000); // Initial refresh interval of 5 seconds
   const identifier = props.identifier;
   const domain = props.domain;
   const id = props.id;
-
-  const fetchMail = () => {
+  useEffect(() => {
     axios
       .get(
         `https://www.1secmail.com/api/v1/?action=readMessage&login=${identifier}&domain=${domain}&id=${id}`
       )
       .then((res) => {
-        if (res.data) {
-          let tempdate = new Date(res.data.date);
-          setTime(
-            new Date(
-              Date.UTC(
-                tempdate.getFullYear(),
-                tempdate.getMonth(),
-                tempdate.getDate(),
-                tempdate.getHours() - 1,
-                tempdate.getMinutes(),
-                tempdate.getSeconds()
-              )
-            ).toLocaleString()
-          );
-          setFrom(res.data.from);
-          setSubject(res.data.subject);
-          setBody(res.data.body);
-          setData(res.data.attachments);
-          // Set refresh interval to 20 seconds if new mail is received
-          setRefreshInterval(20000);
-        } else {
-          // If no mail is received, set refresh interval to 5 seconds
-          setRefreshInterval(5000);
-        }
+        let tempdate = new Date(res.data.date);
+        setTime(
+          new Date(
+            Date.UTC(
+              tempdate.getFullYear(),
+              tempdate.getMonth(),
+              tempdate.getDate(),
+              tempdate.getHours() - 1,
+              tempdate.getMinutes(),
+              tempdate.getSeconds()
+            )
+          ).toLocaleString()
+        );
+        setFrom(res.data.from);
+        setSubject(res.data.subject);
+        setBody(res.data.body);
+        setData(res.data.attachments);
       })
       .catch((err) => {
         console.log(err);
       });
-  };
-
-  useEffect(() => {
-    // Fetch mail on component mount
-    fetchMail();
-
-    // Set up interval to periodically check for new mail
-    const intervalId = setInterval(() => {
-      fetchMail();
-    }, refreshInterval);
-
-    // Clean up interval on component unmount
-    return () => clearInterval(intervalId);
-  }, [domain, identifier, id, refreshInterval]);
+  }, [domain, identifier, id]);
 
   return (
     <div className="mx-5 my-2">
